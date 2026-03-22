3b0dfbb0f91f358de2b7aa1cfb76af01f14cda07
ganeshlohariwal896-maker <ganeshlohariwal896@gmail.com>
2026-03-22 08:21:39 +0530

    Add notification on script connection

---
+++ Myscript.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Script Connected!";
+    Text = "Your GitHub script is running successfully.";
+    Icon = "rbxassetid://6023456881"; -- This adds a little checkmark icon
+    Duration = 10;
+})
